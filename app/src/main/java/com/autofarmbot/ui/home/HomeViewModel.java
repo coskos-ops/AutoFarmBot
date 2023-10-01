4295ff28aa8f478da8a2b7c4ed942fabef661b57
Krishanth Suthaharan <krishanth2001@live.com>
2023-09-30 20:34:48 -0400

    A very, very important update

--- app/src/main/java/com/autofarmbot/ui/home/HomeViewModel.java
+++ app/src/main/java/com/autofarmbot/ui/home/HomeViewModel.java
@@ -10,7 +10,7 @@ public class HomeViewModel extends ViewModel {
 
     public HomeViewModel() {
         mText = new MutableLiveData<>();
-        mText.setValue("Welcome to AutoFarmBot");
+        mText.setValue("Welcome to HydroBot");
     }
 
     public LiveData<String> getText() {
