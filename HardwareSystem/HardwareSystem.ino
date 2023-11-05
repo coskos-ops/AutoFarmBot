ab59dd627ec2c47b34d3614d296ffc9ee03cfe00
Git-Gomes <83992636+Git-Gomes@users.noreply.github.com>
2023-11-05 00:57:28 -0400

    added pH & EC sensing

--- HardwareSystem/HardwareSystem.ino
+++ HardwareSystem/HardwareSystem.ino
@@ -2,16 +2,23 @@
 #include <WiFi.h>
 #include <HTTPClient.h>
 #include <ArduinoJson.h>
+
+const int NUM_SENSORS = 3;
+const int PH_SAMPLES = 10;
+//DEFINE PINOUTS
 const int TRIG_PIN = 12;
 const int ECHO_PIN = 14;
+const int PO_PIN = 33;
+const int TDS_PIN = 32;
+const int TEMP_PIN = 35;
 
 SR04 sr04 = SR04(ECHO_PIN,TRIG_PIN);
-long a;
 
+//DEFINE NETWORK
+const char* ssid = "BELL123";
+const char* password = "21EA45AA79AE";
+String serverName = "https://coskos.pythonanywhere.com/postSensors";
 
-const char* ssid = "Kostic";
-const char* password = "";
-String serverName = "https://coskos.pythonanywhere.com/postDistance";
 void setup() {
   Serial.begin(115200); 
   
@@ -26,34 +33,65 @@ void setup() {
   Serial.println(WiFi.localIP());
 }
 
+float get_pH(int pinout, int samples) {
+  int measurings = 0;
+  for (int i = 0; i < samples; i++){
+    measurings += analogRead(pinout);
+    delay(10);
+  }
+  return ((measurings/samples/4096)*14) - 1.2;
+}
+
+int get_TDS(int pinout) {
+  return analogRead(pinout);
+}
+
+float get_Temp(int pinout) {
+  return analogRead(pinout)*0.48828125;
+}
+
 void loop() {
+  //Get Distance Sensor Data
   long distance = sr04.Distance();
+  //Get pH Sensor Data
+  float pH = get_pH(PO_PIN, PH_SAMPLES);
+  //Get EC Sensor Data
+  int tds = get_TDS(TDS_PIN);
+  //Get Temperature Sensor Data
+  float temperature = get_Temp(TEMP_PIN);
+  //EC [mS/cm^2] = TDS/500
+  float ec = tds/500.0;
+  //Temperature Compensation
+  //for every degree above 25C the TDS will be 2% larger (compounded)
+  int tempApprx = round(temperature);
+  if (tempApprx > 25) {
+    while (tempApprx-- > 25) {
+      ec *= 0.98;
+    }
+  }
+  else if (tempApprx < 25) {
+    while (tempApprx++ < 25) {
+      ec *= 1.02;
+    }
+  }
+
   HTTPClient http;
   http.begin(serverName); 
   http.addHeader("Content-Type", "application/json");
   // http.addHeader("Accept", "*//*");
   // http.addHeader("User-Agend", "Content-Type: application/json);
-  const int capacity = JSON_OBJECT_SIZE(1);
-  StaticJsonDocument<capacity> doc;
+  const int capacity = JSON_OBJECT_SIZE(NUM_SENSORS);
+  DynamicJsonDocument doc(capacity);
 
   doc["distance"] = distance;
+  doc["pH"] = pH;
+  doc["ec"] = ec;
   String json;
   serializeJson(doc, json);
   Serial.println(json);
 
-
-
-
-  
-
   int httpResponseCode = http.POST(json); //Send the actual POST request
   Serial.println(http.getString());
-
-
   http.end();
-
-
-
-   delay(1000);
-  //}
+    delay(500);
 }
