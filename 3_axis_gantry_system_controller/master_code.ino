5cd0e8a5d5de6237bc64fbcd06f263d44a8afde6
Krishanth Suthaharan <krishanth2001@live.com>
2024-03-24 19:34:34 -0400

    gantry with moving claw

--- 3_axis_gantry_system_controller/master_code.ino
+++ 3_axis_gantry_system_controller/master_code.ino
@@ -1,15 +1,25 @@
 #include <AccelStepper.h>
+#include <Servo.h>
 
 // Reference Table for Stepper Distances of each plant with respect to Collection bin (position 0)
 // Assumed ordering (motor is refering to XY direction - written twice but the same):
 // Motor    Plant 1     Plant 3     Plant 5
 // Motor    Plant 2     Plant 4     Plant 6
-
+/*
 const int x1 = 600;
 const int x2 = 1700;
 const int x3 = 3000;
 const int y1 = 600;
 const int y2 = 1200;
+*/
+
+const int x1 = 75;
+const int x2 = 725;
+const int x3 = 1400;
+const int y1 = 400;
+const int y2 = 1150;
+const int plant_z = 1625;
+
 const int plant_1_x = x1; // theoretically determined for now
 const int plant_1_y = y1; // need to confirm in person (Logan or Kris)
 const int plant_2_x = x1; // thought of as a rectangular box
@@ -22,7 +32,7 @@ const int plant_5_x = x3;
 const int plant_5_y = y1;
 const int plant_6_x = x3;
 const int plant_6_y = y2;
-const int plant_z = 1900; //location where cut occurs (2-3 cm from base)
+//const int plant_z = 1900; //location where cut occurs (2-3 cm from base)
 
 // Motor pin configurations
 const int X_step_pin = 2;
@@ -31,11 +41,13 @@ const int Y_step_pin = 3;
 const int Y_dir_pin = 6;
 const int Z_step_pin = 4;
 const int Z_dir_pin = 7;
+const int claw_pin = 9;
 
 // User define steps per second for all motors
-const int motorSpeed = 400;
+const int motorSpeed = 80;
 const int maxSpeed = 500;
 const int acceleration = 100;
+const int scanDelay = 60000; // will check all plants for maturity (in ms)
 //int locations [6] = {2,3,5,1,4,6}; for testing purposes
 
 // debug mode
@@ -50,10 +62,15 @@ bool plant = false;
 int plantID = 1; // values range from 1 to 6
 int tx_rx_ID = 0;
 String prevRecMessage = "empty";
+unsigned long currentTime = 0;
+unsigned long previousTime = 0;
+int pos = 0;    // variable to store the servo position
 
 AccelStepper stepperX(1, X_step_pin, X_dir_pin);
 AccelStepper stepperY(1, Y_step_pin, Y_dir_pin);
 AccelStepper stepperZ(1, Z_step_pin, Z_dir_pin);
+Servo myservo;  // create servo object to control a servo
+// twelve servo objects can be created on most boards
 
 void setup() {
   pinMode(X_step_pin, OUTPUT);
@@ -64,6 +81,8 @@ void setup() {
   pinMode(Z_dir_pin, OUTPUT);
 
   Serial.begin(9600); // for communications 
+  
+  myservo.attach(claw_pin);  // attaches the servo on pin 9 to the servo object
   
   stepperX.setMaxSpeed(maxSpeed);
   stepperY.setMaxSpeed(maxSpeed);
@@ -81,7 +100,9 @@ void setup() {
   // all operations should reset to this point
   stepperX.runToNewPosition(0);
   stepperY.runToNewPosition(0);
-  stepperZ.runToNewPosition(0);   
+  stepperZ.runToNewPosition(0); 
+
+  close_claw();
 }
 
 void loop() {
@@ -100,7 +121,7 @@ void loop() {
 
       Tranmit: Busy/Ready
   */
-
+  
   while(1){
   if (Serial.available() > 0) {
     String receivedMessage = Serial.readStringUntil('\n');
@@ -122,25 +143,31 @@ void loop() {
     if(verbose) printDebug("Plant Number ", plantID);
     if(verbose) printDebug("TX RX IDENTITY ", tx_rx_ID);
 
-    /*
-        Based on comms, translate into position and move 3D gantry
-    */
     
+    //    Based on comms, translate into position and move 3D gantry
     if(harvest){
       if(verbose) Serial.print("Harvesting in progress \n");
       
       // go to plant
       stepperY.runToNewPosition(plantIDtoYposition(plantID));
       stepperX.runToNewPosition(plantIDtoXposition(plantID));
+      // signal to servo motor (sg90) -> OPEN
+      open_claw();
       stepperZ.runToNewPosition(plant_z);
-      stepperZ.runToNewPosition(plant_z-500);
+      close_claw();
+      
+      //stepperZ.runToNewPosition(plant_z-500);
 
-      // cut and grab plant (TBD KRIS or LOGAN) - signal to servo motor (sg90)
+      // reset position
+      stepperZ.runToNewPosition(0);
+      stepperY.runToNewPosition(0);
+      stepperX.runToNewPosition(0);
 
-      // drop plant in container (TBD KRIS) and reset position
-      //stepperZ.runToNewPosition(0);
-      //stepperY.runToNewPosition(0);
-      //stepperX.runToNewPosition(0);
+      // drop plant in container (TBD KRIS) 
+      // signal to servo motor (sg90) -> OPEN
+      open_claw();
+      //close
+      close_claw();
       
       // reset flags
       ready = true;
@@ -178,10 +205,71 @@ void loop() {
     //if(verbose) delay(300); // 0.3 second delay
     prevRecMessage = receivedMessage;
   }
+    // code to check plant height
+    currentTime = millis(); // in milliseconds
+
+    // is it time yet?
+    if(currentTime - previousTime >= scanDelay){
+      // code to scan
+      if(verbose) Serial.print("Gantry Scanning to check for Plant Maturity\n");
+      scan_for_plant_maturity();
+      previousTime = currentTime;
+    }
   }
+
   //reset();
   if(verbose) Serial.print("Program Closed\n");
   //exit(0);
+}
+void close_claw(){
+  for (pos = 50; pos <= 120; pos += 1) { // goes from 0 degrees to 180 degrees
+    // in steps of 1 degree
+    myservo.write(pos);              // tell servo to go to position in variable 'pos'
+    delay(15);                       // waits 15ms for the servo to reach the position
+  }
+}
+
+void open_claw(){
+  for (pos = 120; pos >= 50; pos -= 1) { // goes from 180 degrees to 0 degrees
+    myservo.write(pos);              // tell servo to go to position in variable 'pos'
+    delay(15);                       // waits 15ms for the servo to reach the position
+  }
+}
+
+void scan_for_plant_maturity(){
+  stepperY.runToNewPosition(plantIDtoYposition(5));
+  stepperX.runToNewPosition(plantIDtoXposition(5));
+  //stepperZ.runToNewPosition(plant_z);
+  stepperZ.runToNewPosition(plant_z-500);
+  //delay(3000);
+  stepperY.runToNewPosition(plantIDtoYposition(3));
+  stepperX.runToNewPosition(plantIDtoXposition(3));
+  //stepperZ.runToNewPosition(plant_z);
+  stepperZ.runToNewPosition(plant_z-500);
+  //delay(3000);
+  stepperY.runToNewPosition(plantIDtoYposition(1));
+  stepperX.runToNewPosition(plantIDtoXposition(1));
+  //stepperZ.runToNewPosition(plant_z);
+  stepperZ.runToNewPosition(plant_z-500);
+  //delay(3000);
+  stepperY.runToNewPosition(plantIDtoYposition(2));
+  stepperX.runToNewPosition(plantIDtoXposition(2));
+  //stepperZ.runToNewPosition(plant_z);
+  stepperZ.runToNewPosition(plant_z-500);
+  //delay(3000);
+  stepperY.runToNewPosition(plantIDtoYposition(4));
+  stepperX.runToNewPosition(plantIDtoXposition(4));
+  //stepperZ.runToNewPosition(plant_z);
+  stepperZ.runToNewPosition(plant_z-500);
+  //delay(3000);
+  stepperY.runToNewPosition(plantIDtoYposition(6));
+  stepperX.runToNewPosition(plantIDtoXposition(6));
+  //stepperZ.runToNewPosition(plant_z);
+  stepperZ.runToNewPosition(plant_z-500);
+  //delay(3000);
+  stepperX.runToNewPosition(0);
+  stepperY.runToNewPosition(0);
+  stepperZ.runToNewPosition(0); 
 }
 
 void reset (){
